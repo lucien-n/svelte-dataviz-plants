e507bb09be1a4160644d2830916bb39145e17c7e
scaffus <scaffus.scaffus@gmail.com>
2023-03-22 17:01:44 +0100

    plants

---
+++ src/lib/PlantCard.svelte
@@ -0,0 +1,14 @@
+<script>
+	export let data;
+</script>
+
+<section id="plant-{data.common_name}">
+	<p>Common Name: {data.common_name}</p>
+	<p>Scientific Name: {data.scientific_name}</p>
+</section>
+
+<style scoped>
+	section {
+		border: 2px solid black;
+	}
+</style>
