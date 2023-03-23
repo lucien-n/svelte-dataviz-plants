9bcbae3ead51c88f52472b0ccdf7f4ecdc448167
Lucien Neuhoff <77048269+lucien-neuhoff@users.noreply.github.com>
2023-03-23 12:11:05 +0100

    local plants data and grid display

--- src/App.svelte
+++ src/App.svelte
@@ -2,18 +2,22 @@
 	import './app.css';
 
 	import PlantCard from './lib/PlantCard.svelte';
+	import { plants } from './data';
 
 	async function getPlants() {
-		const data = await fetch(
-			'https://perenual.com/api/species-list?page=1&key=sk-9lep6419bee20788e274'
-		);
-		const plants = await data.json();
+		// const data = await fetch(
+		// 	'https://perenual.com/api/species-list?page=1&key=sk-pHQb641c2838df533292'
+		// );
+		// const plants = await data.json();
 		return plants;
 	}
 </script>
 
-<main>
-	<section id="plants-cards">
+<main class="w-full h-full">
+	<section
+		id="plants-cards"
+		class="grid gap-3 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-5 w-full md:w-3/5 mx-auto"
+	>
 		{#await getPlants() then plants}
 			{#each plants.data as plant}
 				<PlantCard data={plant} />
@@ -21,3 +25,9 @@
 		{/await}
 	</section>
 </main>
+
+<style>
+	main {
+		background-color: #e9f0e2;
+	}
+</style>
--- src/lib/PlantCard.svelte
+++ src/lib/PlantCard.svelte
@@ -4,25 +4,25 @@
 	let image_url =
 		data.default_image && data.default_image.original_url != null
 			? data.default_image.original_url
-			: 'https://placeholder.pics/svg/300';
+			: 'https://placeholder.pics/svg/100';
 </script>
 
 <section
 	id="plant-{data.common_name}"
-	class="mx-auto flex border-zinc-500 border rounded-md h-1/6"
+	class=" bg-white border-zinc-500 border rounded-md h-full hover:cursor-pointer shadow-md transition ease-in-out duration-300 hover:shadow-2xl hover:border-zinc-00"
 >
-	<div
-		class="flex w-2/5 overflow-hidden rounded-md border border-b-0 border-zinc-700 shadow-lg"
-	>
+	<div class="h-2/3 overflow-hidden rounded-t-md shadow-lg">
 		<img
 			src={image_url}
 			alt=""
-			class="w-full h-full self-center rounded-lg rounded-r-none hover:cursor-pointer"
+			class="w-full h-full self-center hover:cursor-pointer"
 			style="object-fit: cover;"
 		/>
 	</div>
 	<div class="p-2">
-		<p>Common Name: {data.common_name}</p>
-		<p>Scientific Name: {data.scientific_name}</p>
+		<p class="font-semibold text-md">{data.common_name}</p>
+		<p class="italic text-zinc-500">
+			{data.scientific_name}
+		</p>
 	</div>
 </section>
